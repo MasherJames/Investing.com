85cca00dec37573fe0813caf821c0823b759fb46
James Macharia <jamesmasher13@gmail.com>
2020-06-19 00:36:02 +0300

    Add models

--- app/db/models.py
+++ app/db/models.py
@@ -1,2 +1,48 @@
-class Company:
-    pass
+from sqlalchemy import Column, Integer, String, Float
+from .database import Base
+
+
+class Company(Base):
+    __tablename__ = "companies"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True, nullable=False)
+
+    def __init__(self, name=None):
+        ''' Company constructor '''
+        self.name = name
+
+    def __repr__(self):
+        ''' Object representation on a company '''
+        return '<Company %r>' % (self.name)
+
+
+class HistoricalData(Base):
+    __tablename__ = "historicaldata"
+    id = Column(Integer, primary_key=True)
+    date = Column(String(50), nullable=False)
+    price = Column(Float, nullable=False)
+    open_price = Column(Float, nullable=False)
+    highest_price = Column(Float, nullable=False)
+    lowest_price = Column(Float, nullable=False)
+    volume = Column(String(50), nullable=False)
+    percentage_change = Column(String(50), nullable=False)
+
+    def __init__(self, date=None,
+                 price=None,
+                 open_price=None,
+                 highest_price=None,
+                 lowest_price=None,
+                 volume=None,
+                 percentage_change=None,):
+        ''' HistoricalData constructor '''
+        self.date = date
+        self.price = price
+        self.open_price = open_price
+        self.highest_price = highest_price
+        self.lowest_price = lowest_price
+        self.volume = volume
+        self.percentage_change = percentage_change
+
+    def __repr__(self):
+        ''' Object representation on historical data '''
+        return '<HistoricalData %r>' % (self.date)
---
+++ app/utils/serializer.py
@@ -0,0 +1,7 @@
+def serialize_company():
+    ''' Custom company serializer '''
+    pass
+
+
+def serializer_historical_data():
+    pass
