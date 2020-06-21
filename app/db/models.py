192e5dd012097a45b7ea598d4d53952264ceda9e
James Macharia <jamesmasher13@gmail.com>
2020-06-21 12:45:22 +0300

    Fix relationship import

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey, relationship
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
 from .database import Base
 
 
