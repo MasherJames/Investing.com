56f91c03c7a8779e4dc520d1ec8f25589b07e8c7
James Macharia <jamesmasher13@gmail.com>
2020-06-29 03:05:05 +0300

    Add app serializers

--- app/utils/serializer.py
+++ app/utils/serializer.py
@@ -1,7 +1,22 @@
-def serialize_company():
+def serialize_company(company):
     ''' Custom company serializer '''
-    pass
+    return {
+        "id": company.id,
+        "name": company.name
+    }
 
 
-def serializer_historical_data():
-    pass
+def serializer_historical_data(data):
+    ''' Serialize historical data '''
+
+    return {
+        "id": data.id,
+        "date": data.date,
+        "price": data.price,
+        "open_price": data.open_price,
+        "highest_price": data.highest_price,
+        "lowest_price": data.lowest_price,
+        "volume": data.volume,
+        "percentage_change": data.percentage_change,
+        "company": serialize_company(data.company)
+    }
