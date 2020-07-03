b32c4172b19af555ee10856cc925bf8ac495eeac
James Macharia <jamesmasher13@gmail.com>
2020-07-03 15:21:59 +0300

    Add more date utils

--- app/utils/date.py
+++ app/utils/date.py
@@ -1,6 +1,17 @@
-from datetime import datetime
+from datetime import datetime, date, timedelta
+
+today = date.today()
+sevenDaysAgo = today - timedelta(days=7)
 
 
 def format_date(date_string, date_format):
     date_obj = datetime.strptime(date_string, "%B %d, %Y")
     return date_obj.strftime(date_format)
+
+
+def start_date():
+    return sevenDaysAgo.strftime("%m/%d/%Y")
+
+
+def end_date():
+    return today.strftime("%m/%d/%Y")
