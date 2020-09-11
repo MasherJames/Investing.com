5c53b01eac866596dd065f123c33d8e044ad96e7
James Macharia <jamesmasher13@gmail.com>
2020-09-11 14:49:47 +0300

    Fix date formatter

--- app/utils/date.py
+++ app/utils/date.py
@@ -2,5 +2,5 @@ from datetime import datetime
 
 
 def format_date(date_string, date_format):
-    date_obj = datetime.strptime(date_string, "%B %d, %Y")
+    date_obj = datetime.strptime(date_string, "%b %d, %Y")
     return date_obj.strftime(date_format)
